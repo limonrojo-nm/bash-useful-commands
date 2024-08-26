1a2f381777ed2f1dec024919d1965eb84d5b6a1b
Nicolas Miñan <63977566+limonrojo-nm@users.noreply.github.com>
2024-08-26 11:45:02 -0300

    Create _lib.sh

---
+++ _lib.sh
@@ -0,0 +1,23 @@
+normal=$(tput sgr0)
+
+bold=$(tput bold)
+underline=$(tput smul)
+nounderline=$(tput rmul)
+reverse=$(tput rev)
+
+black=$(tput setaf 0)
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+blue=$(tput setaf 4)
+magenta=$(tput setaf 5)
+cyan=$(tput setaf 6)
+white=$(tput setaf 7)
+
+
+
+# echo "${bold}NEGRITA${normal}"
+# echo "${underline}Subrayado${nounderline}"
+# echo "${reverse}Revertido${normal}"
+# echo "${cyan}cyan${normal}"
+
